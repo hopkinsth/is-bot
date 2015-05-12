588eb422aaa836ef4d01c0bf5f74e59f59949752
Thomas Hopkins <thopkins@redventures.com>
2015-05-12 11:18:36 -0400

    escape the dot

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var pattern = new RegExp('bot|crawl|spider|borg|yahoo|slurp|archiver|netresearch
 |gomeza|google sketchup|read later|pingdom|facebook|rackspace|scan|link|ezine|preview\
 |dig|tarantula|urllib|jakarta|wget|rget|monitor|libwww|moozilla|seer|spice|snoopy|feedfetcher\
 |google wireless transcoder|curl|wordpress|java|netfront|archive|xenu|feed|appmanager|covario\
-|perl|host|lwp|page speed|ptst|digext|nutch|sleuth|yottaamonitor|arbiter.js', 'i');
+|perl|host|lwp|page speed|ptst|digext|nutch|sleuth|yottaamonitor|arbiter\\.js', 'i');
 
 /**
  * Determines if `useragent` is a bot according to `pattern`.
--- test.js
+++ test.js
@@ -7,3 +7,4 @@ assert(isbot('AdsBot-Google (+http://www.google.com/adsbot.html)'));
 assert(!isbot('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36'));
 assert(!isbot(''));
 assert(isbot('Arbiter.js/1.0'));
+assert(!isbot('arbiterjs'));
